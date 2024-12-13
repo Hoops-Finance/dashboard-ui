e9c5ee2a7138bf6dee1af5473f0456196448b102
Iver Romero <iverlandth@gmail.com>
2024-12-13 14:05:46 -0400

    fix: #11 add loading image

--- components/auth/socialAuth.tsx
+++ components/auth/socialAuth.tsx
@@ -77,12 +77,32 @@ export default function SocialAuth() {
           <div className="rounded-xl border bg-card text-card-foreground shadow">
             {authSuccess ? (
               <div>
-                <h1>Authenticating...</h1>
+                <h1>
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </h1>
               </div>
             ) : (
               <div>
                 <h1> An error occurred while attempting to authenticate your account with Google or Discord </h1>
-
                 <div>
                   <div>
                     <Link href={socialLogin}>Please try again</Link>
