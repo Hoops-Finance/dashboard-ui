620b301cbf132115d0a2a4b7e832b9d238e33e94
Iver Romero <iverlandth@gmail.com>
2024-12-13 13:36:37 -0400

    updating build error

--- app/auth/callback/google/page.tsx
+++ app/auth/callback/google/page.tsx
@@ -1,6 +1,15 @@
 //component imports
 import SocialAuth from "@/components/auth/socialAuth";
+import { Suspense } from "react";
 
-export default function SocialAuthPage() {
+function SocialAuthPage() {
   return <SocialAuth />;
+}
+
+export default function SuspenseWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SocialAuthPage />
+    </Suspense>
+  );
 }
