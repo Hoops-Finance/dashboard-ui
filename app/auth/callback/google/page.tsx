5f887580901d9a0b997b3b5b49ef5dcf16c825e6
Timothy Baker <35656692+silence48@users.noreply.github.com>
2024-12-13 04:45:37 -0500

    add suspense boundry

--- app/auth/callback/google/page.tsx
+++ app/auth/callback/google/page.tsx
@@ -1,6 +1,10 @@
-//component imports
+import { Suspense } from 'react';
 import SocialAuth from "@/components/auth/socialAuth";
 
 export default function SocialAuthPage() {
-  return <SocialAuth />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SocialAuth />
+    </Suspense>
+  );
 }
