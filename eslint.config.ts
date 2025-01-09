0158ce57dd8a284efc95863326e8f8e449bae529
Timothy Baker <35656692+silence48@users.noreply.github.com>
2025-01-09 15:47:06 -0500

    update eslint

---
+++ eslint.config.ts
@@ -0,0 +1,59 @@
+import { Linter } from "eslint";
+import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next"; 
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+//  import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+const myConfig: Linter.Config = {
+  name: "default-config",
+  rules: {
+    "standard/array-bracket-even-spacing": "off",
+    "standard/computed-property-even-spacing": "off",
+    "standard/object-curly-even-spacing": "off",
+    "@typescript-eslint/block-spacing": "off",
+    "@typescript-eslint/brace-style": "off",
+    "@typescript-eslint/comma-dangle": "off",
+    "@typescript-eslint/comma-spacing": "off",
+    "@typescript-eslint/func-call-spacing": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/key-spacing": "off",
+    "@typescript-eslint/keyword-spacing": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/no-extra-parens": "off",
+    "@typescript-eslint/no-extra-semi": "off",
+    "@typescript-eslint/object-curly-spacing": "off",
+    "@typescript-eslint/semi": "off",
+    "@typescript-eslint/space-before-blocks": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "@typescript-eslint/space-infix-ops": "off",
+    "@typescript-eslint/type-annotation-spacing": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unnecessary-type-parameters": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/restrict-template-expressions": "off", //this has many warnings fix it later
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-extraneous-class": "off"
+  }
+};
+
+const allconfigs = tseslint.config(
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  { languageOptions: { parserOptions: { project: true } } },
+  eslintPluginPrettierRecommended,
+  {
+    files: ["./","src/**/*.ts", "src/**/*.tsx"],
+     plugins: {
+      "@next/eslint-plugin-next": nextPlugin
+     }
+  },
+  myConfig,
+  {
+    ignores: [".history", ".vscode", "node_modules", "dist", "build", "tests", ".next", "out"]
+  }
+);
+
+export default [...allconfigs];
