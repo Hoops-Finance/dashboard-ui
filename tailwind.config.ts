51f28b4c7cb04b80f1fc63a1bbe903aa58182b86
Timothy Baker <35656692+silence48@users.noreply.github.com>
2025-02-11 02:09:35 -0500

    update tailwind config and add a eslint js file for next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,13 @@ const config: Config = {
   content: [
     "**/*.{js,ts,jsx,tsx,mdx}", // Include all relevant files
     "!node_modules/**/*", // Exclude node_modules
-    "!dist/**/*" // Exclude dist/build folders
+    "!dist/**/*", // Exclude dist/build folders
   ],
   theme: {
     extend: {
       fontFamily: {
         lora: ["Lora", "serif"],
-        inter: ["Inter", "sans-serif"]
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
         yellow: { DEFAULT: "#F7D423" },
@@ -22,31 +22,31 @@ const config: Config = {
         foreground: "hsl(var(--foreground))",
         card: {
           DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))"
+          foreground: "hsl(var(--card-foreground))",
         },
         popover: {
           DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))"
+          foreground: "hsl(var(--popover-foreground))",
         },
         primary: {
           DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))"
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))"
+          foreground: "hsl(var(--secondary-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))"
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))"
+          foreground: "hsl(var(--accent-foreground))",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))"
+          foreground: "hsl(var(--destructive-foreground))",
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -56,8 +56,8 @@ const config: Config = {
           2: "hsl(var(--chart-2))",
           3: "hsl(var(--chart-3))",
           4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))"
-        }
+          5: "hsl(var(--chart-5))",
+        },
       },
       screens: {
         mobile: { max: "480px" },
@@ -65,16 +65,16 @@ const config: Config = {
         tablet: { max: "843px" },
         "tablet-landscape": { max: "1024px" },
         laptop: { max: "1440px" },
-        desktop: { max: "1440px" }
+        desktop: { max: "1440px" },
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)"
-      }
-    }
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
-  plugins: [tailwindAnimate]
+  plugins: [tailwindAnimate],
 };
 
 export default config;
